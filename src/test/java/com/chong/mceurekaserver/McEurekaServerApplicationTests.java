50a8e1b14df02f6c5ca71fb30c48d513ed2ac2e2
chongpengfei <chongpengfei@jusstech.com>
2020-02-10 20:49:56 +0800

    first commit

---
+++ src/test/java/com/chong/mceurekaserver/McEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chong.mceurekaserver;
+
+//import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assert (true);
+    }
+
+}
